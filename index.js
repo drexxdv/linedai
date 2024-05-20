f9d9f7abb5af930dbf69d2932316aa7ef0e8c625
GhostyDev <152715191+drexxdv@users.noreply.github.com>
2024-05-20 19:00:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const path = require('path');
 
+// Asumiendo que tu archivo app.js se encuentra en la raíz del proyecto
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
 app.get('/', async (req, res) => {
